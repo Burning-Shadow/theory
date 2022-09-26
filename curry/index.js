8d249fec605ac45171b6d29c9f3f9499004bc059
francisswang <2485999859@qq.com>
2022-09-26 15:46:19 +0800

    curry

--- curry/index.js
+++ curry/index.js
@@ -1,50 +1,44 @@
-function add() {
-  // 第一次执行时，定义一个数组专门用来存储所有的参数
-  var _args = Array.prototype.slice.call(arguments);
-
-  // 在内部声明一个函数，利用闭包的特性保存_args并收集所有的参数值
-  var _adder = function () {
-    _args.push(...arguments);
-    return _adder;
-  };
-
-  // 利用toString隐式转换的特性，当最后执行时隐式转换，并计算最终的值返回
-  _adder.toString = function () {
-    return _args.reduce(function (a, b) {
-      return a + b;
-    });
-  }
-  return _adder;
+function sum(a, b, c) {
+  // console.log('arguments = ', arguments);
+  return a + b + c;
 }
 
-console.log(add(1)(2)(3).toString()) // 6
-console.log(add(1, 2, 3)(4).toString()) // 10
-console.log(add(1)(2)(3)(4)(5).toString()) // 15
-console.log(add(2, 6)(1).toString()) // 9
-console.log(add(1, 2, 3, 4, 5, 6, 7).toString()) // 28
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> callCurry >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
+function callCurry(func) {
+  return function curried(...args) {
+    if (args.length >= func.length) {
+      return func.apply(this, args);
+    } else {
+      return function (...args2) {
+        return curried.apply(this, args.concat(args2));
+      }
+    }
+  };
+};
 
-// function add() {
-//   // 第一次执行时，定义一个数组专门用来存储所有的参数
-//   var _args = Array.prototype.slice.call(arguments);
+const currySum = callCurry(sum);
 
-//   // 在内部声明一个函数，利用闭包的特性保存_args并收集所有的参数值
-//   var _adder = function () {
-//     _args.push(...arguments);
-//     return _adder;
-//   };
+console.log(currySum(1, 2, 3));
+console.log(currySum(1)(2, 3));
+console.log(currySum(1)(2)(3));
 
-//   // 利用toString隐式转换的特性，当最后执行时隐式转换，并计算最终的值返回
-//   _adder.toString = function () {
-//     return _args.reduce(function (a, b) {
-//       return a + +b; // +b 隐式转换
-//     });
-//   }
-//   return _adder;
-// }
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
-// console.log('' + add(1)(2)(3)) // 6
-// console.log('' + add(1, 2, 3)(4)) // 10
-// console.log('' + add(1)(2)(3)(4)(5)) // 15
-// console.log('' + add(2, 6)(1)) // 9
-// console.log('' + add(1, 2, 3, 4, 5, 6, 7)) // 28
+function bindCurry(func) {
+  return function bindfn() {
+    if (arguments.length < func.length) {
+      console.log('bind');
+      return bindfn.bind(null, ...arguments);
+      // 关键：保存参数，并在调用时和后面的参数一起传入 bindfn
+    }
+
+    return func.apply(null, arguments);
+  }
+}
+
+const bindCurrySum = bindCurry(sum);
+
+console.log(bindCurrySum(1, 2, 3));
+console.log(bindCurrySum(1)(2, 3));
+console.log(bindCurrySum(1)(2)(3));
